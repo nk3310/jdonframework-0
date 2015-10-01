cd323e543abff60e07ae77d72a7cac8c00fd8611
nk3310 <nk3310@126.com>
2015-10-01 09:43:02 +0800

    项目启动和编辑的批处理文件修改

--- example/jdonMVC+CQRS+ES/other/3-jdonMVC+CQRS+ES.bat
+++ example/jdonMVC+CQRS+ES/other/3-jdonMVC+CQRS+ES.bat
@@ -6,7 +6,6 @@ CD E:\h2\bin
 
 start E:\eclipse\indigo\eclipse\eclipse.exe -data D:\workspace\workspace37\jdon
 start explorer D:\src\git\nk3310@github\jdonframework\example\jdonMVC+CQRS+ES
-start explorer D:\workspace\workspace37\jdon\jdonmvc_cqrs_es
 start explorer E:\jetty\logs
 start explorer http://localhost:8080/jdonmvc_cqrs_es-1.0/
 
